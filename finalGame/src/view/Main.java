02bd0f2ca4a44e6b053cd5a9412f58560a12b024
Laura Orozco <69904799+Melilaura@users.noreply.github.com>
2021-06-12 17:18:54 -0500

    added the sort of scores

--- finalGame/src/view/Main.java
+++ finalGame/src/view/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.*;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -87,7 +88,11 @@ public class Main extends PApplet {
 		saveScore = new String[5];
 		saveDate = new String[5];
 		
+		
+		
 		list = new ArrayList<>();
+		
+		separateScores();
 
 		///// TIMER///////
 		s = 0;
@@ -97,7 +102,7 @@ public class Main extends PApplet {
 
 		// background = loadImage("land.jpg");
 		landscapeX = 0;
-		stageNum = -1;
+		stageNum = 4;
 		subStageNum = 0;
 		score = 0;
 
@@ -180,7 +185,7 @@ public class Main extends PApplet {
 		noStroke();
 		rectMode(CORNER);
 		imageMode(CORNER);
-
+		
 		loadStages();
 
 		switch (stageNum) {
@@ -640,8 +645,10 @@ public class Main extends PApplet {
 
 					for (int i = 0; i < lines.length; i++) {
 						lines[i] = scoreStrings[i];
+						
 					}
 
+					
 					System.out.println(lines.length);
 					saveStrings("./data/highScore.txt", scoreStrings); // guarda los puntajes
 					// }
@@ -679,80 +686,33 @@ public class Main extends PApplet {
 
 		switch (key) {
 		
-		case 'b':
-			//separateScores();
-
+		//sort score 
+		case 'a':
 			
-//			sort(lines);
-//			String scoreStrings[] = { str(score) + " " + userName + " " + date, lines[0], lines[1], lines[2],
-//					lines[3] };
-//
-//			for (int i = 0; i < lines.length; i++) {
-//				String user = userName;
-//				String score1 = str(score);
-//				String date1 = date;
-//				
-//				//String time = timer ;
-//				
-//				String line = score1 + " " + user + " " + date1 ;
-//				
-//				saveName[i] = line;
-//			}
-//
-//			System.out.println(lines.length);
-//			saveStrings("./data/SaveId.txt", saveName); // guarda los puntajes
+			for (int i = 0; i < lines.length; i++) {
+				
+				scores=reverse(lines);
+				 
+			}
+
+			saveStrings("./data/highScore.txt", scores);
+			System.out.println("sort score");
 			
-//			Collections.sort(list);
+			break;
+			
+		case 's':
+			
+		
+		
+		break;
 
-//			for (int i = 0; i < list.size(); i++) {
-//				
-//                String score1 = str(score);
-//				
-//				String user = userName;
-//				
-//				String date1 = date;
-//				
-//				//String time = timer ;
-//				
-//				String line = score1 + " " + user + " " + date1 ;
-//				
-//				saveName[i] = line;
-//				
-//				
-//			}
-//			
-//			saveStrings("./data/SaveId.txt", saveName);
-//			System.out.println("sort name");
+		case 'd':
 
-//		case 's':
-//
-//			sort(lines);
-//
-//			for (int i = 0; i < lines.length; i++) {
-//
-//				String line = date + " " + userName + " " + lines;
-//
-//				saveDate[i] = line;
-//
-//				/// saveStrings(" ", saveDate);
-//			}
-//
-//		case 'd':
-//
-//			sort(lines);
-//
-//			for (int i = 0; i < lines.length; i++) {
-//
-//				String line = lines + " " + userName + " " + date;
-//
-//				saveScore[i] = line;
-//
-//				// saveStrings(" ", saveScore);
-//			}
 
 			break;
 		}
 	}
+	
 
 	public void mousePressed() {
 
@@ -778,6 +738,8 @@ public class Main extends PApplet {
 			text("score " + score, 680, 30);
 			
 			/////////////// TIMER////////////
+			if (stageNum > 0&&stageNum > 4) {
+			
 			if (s <= 59) {
 
 				s = s + 1;
@@ -796,7 +758,7 @@ public class Main extends PApplet {
 				mt = 0;
 			}
 			///////////// TIMER//////////////
-
+			}
 		}
 	}
 
@@ -812,12 +774,12 @@ public class Main extends PApplet {
 			String score = Line1[0];
 			String user = Line1[1];
 			String date = Line1[2];
-			//String time = Line1[3];
+			String time = Line1[3];
 			
 			list.add(score) ;
 			list.add(user) ;
 			list.add(date) ;
-			//list.add(time) ;
+			list.add(time) ;
 			System.out.println(list);
 			
 			
